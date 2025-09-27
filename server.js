31dcefc236d015bc557cd22eb67eb1a5df9320bf
Helen Martmaa <helen.aitai@gmail.com>
2025-09-27 22:38:28 +0300

    Added second part - sending/presenting the data as JSOn by HTTP web server

--- server.js
+++ server.js
@@ -2,17 +2,36 @@
 const fs = require("fs");
 const path = require("path");
 const csv = require("csv-parser"); // vajalik csv lugemiseks ja js objektideks tegemiseks
+const express = require("express"); // express serveri jaoks
 
 const DATA_FILE = path.join(__dirname, "LE.txt"); // vajalik, et näidata, kus LE.txt asub ehk server.js-ga samas kaustas
 let parts = []; // array, kuhu importida info LE.txt dokumendist
 
 // Funktsioon andmete lugemiseks 
-function loadData() {
+async function loadData() {
   return new Promise((resolve, reject) => {
     const results = [];
     fs.createReadStream(DATA_FILE)
-      .pipe(csv({ separator: '"' })) // eraldi andmed jutumärkides
+      .pipe(csv({ 
+        separator: '\t',
+        headers: ['seerianumber','toote_nimi','laoj22k1','laoj22k2','laoj22k3','laoj22k4','laoj22k5','col8','hind','m2rkus','hind_koos_km']
+       })) // eraldi andmed jutumärkides
       .on("data", (row) => {
+        // 9. veerg – hind
+        const val9 = row.hind;
+        if (val9 !== undefined && val9 !== null && val9 !== "") {
+          row.hind = parseFloat(val9.replace(",", ".")); // teisenda arvuks
+        } else {
+          row.hind = val9; // säilita originaal
+        }
+
+        // 11. veerg – hind koos km
+        const val11 = row.hind_koos_km;
+        if (val11 !== undefined && val11 !== null && val11 !== "") {
+          row.hind_koos_km = parseFloat(val11.replace(",", ".")); // teisenda arvuks
+        } else {
+          row.hind_koos_km = val11; // säilita originaal
+        }
         results.push(row);
       })
       .on("end", () => {
@@ -24,7 +43,13 @@ function loadData() {
   });
 }
 
-// Laadimine lehe käivitamisel
-loadData().catch((err) => {
-  console.error("Viga faili lugemisel:", err);
+const app = express ();
+//lisame kõik andmed json-i, mis localhostis port 3300
+app.get("/", (req, res) => {
+  res.json(parts);
+});
+
+const PORT=3300;
+loadData().then(() => {
+  app.listen(PORT, () => console.log(`Server töötab http://localhost:${PORT}`));
 });
